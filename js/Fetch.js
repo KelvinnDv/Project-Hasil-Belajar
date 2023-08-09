3e1e18151a418729dfba3c026d6243542bfec27b
kelvin <muhammadfalahalfarisi@gmail.com>
2023-08-09 16:07:56 +0800

    Memindahkan file js

--- js/Fetch.js
+++
@@ -1,9 +0,0 @@
-//* Fetch
-//* Adalah sebuah method pada API javascript untuk mengambil resource dari jaringan dan mengembalikan promise yang selesai (fullfilled) ketika ada response yang tersedia.
-
-//* fetch(resource, init );
-//*  Resource 
-//*   URl
-//*   Request Object 
-
-//* Response adalah hasil fetch berupa  
